43840e52876ce9b81c363688713df1e1b93957ca
Noman5237 <anonyman637@gmail.com>
2021-07-22 21:17:18 +0600

    Exporting modules

--- solutions/19-noman/src/main/java/module-info.java
+++ solutions/19-noman/src/main/java/module-info.java
@@ -2,5 +2,9 @@ module game.tictactoe {
 	requires javafx.controls;
 	requires javafx.graphics;
 	
+	exports game.tictactoe.model;
+	exports game.tictactoe.view;
+	exports game.tictactoe.controller.player;
+	exports game.tictactoe.manager;
 	exports game.tictactoe;
 }
