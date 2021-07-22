938e5cd6ca731e7a33f2425f6895924b5b825c9a
Noman5237 <anonyman637@gmail.com>
2021-07-22 21:15:48 +0600

    #5 UIPlayer aka Human Player

---
+++ solutions/19-noman/src/main/java/game/tictactoe/controller/player/UIPlayer.java
@@ -0,0 +1,32 @@
+package game.tictactoe.controller.player;
+
+import game.tictactoe.manager.GameManager;
+import game.tictactoe.manager.UIManager;
+import game.tictactoe.model.Board;
+import game.tictactoe.model.CellState;
+import game.tictactoe.model.Move;
+import game.tictactoe.view.BoardView;
+import javafx.geometry.Point2D;
+import javafx.scene.transform.NonInvertibleTransformException;
+
+public class UIPlayer extends Player {
+	
+	
+	public UIPlayer(CellState playerSymbol) {
+		super(playerSymbol);
+	}
+	
+	@Override
+	public void promptForNextMove(Board board) {
+		BoardView boardView = UIManager.getInstance().getBoardView();
+		boardView.setOnMouseClicked(mouseEvent -> {
+			try {
+				Point2D point2D = boardView.getAffine().inverseTransform(mouseEvent.getX(), mouseEvent.getY());
+				Move nextMove = new Move(((int) point2D.getX()), ((int) point2D.getY()), super.getPlayerSymbol());
+				GameManager.getInstance().setNextMove(nextMove);
+			} catch (NonInvertibleTransformException ignored) {
+			}
+		});
+	}
+	
+}
