b4ab5c69de282422fb433df066937d0ee17322c9
Noman5237 <anonyman637@gmail.com>
2021-07-22 21:18:35 +0600

    #4 #5 #6 #7 Testing out controllers; game crashes before last move

--- solutions/19-noman/src/main/java/game/tictactoe/MainApp.java
+++ solutions/19-noman/src/main/java/game/tictactoe/MainApp.java
@@ -1,8 +1,10 @@
 package game.tictactoe;
 
-import game.tictactoe.model.Board;
+import game.tictactoe.controller.player.UIPlayer;
+import game.tictactoe.controller.player.ai.RandomAIPlayer;
+import game.tictactoe.manager.GameManager;
+import game.tictactoe.manager.UIManager;
 import game.tictactoe.model.CellState;
-import game.tictactoe.view.BoardView;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -11,17 +13,12 @@ public class MainApp extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Board board = new Board();
-		board.setCellState(0, 0, CellState.O);
-		board.setCellState(0, 1, CellState.X);
-		board.setCellState(1, 1, CellState.X);
-		board.setCellState(2, 0, CellState.X);
-		board.setCellState(0, 2, CellState.O);
-		BoardView boardView = new BoardView(800, 800);
-		boardView.draw(board);
-		Scene scene = new Scene(boardView);
+		Scene scene = new Scene(UIManager.getInstance().getSceneRoot());
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("JavaFX");
 		primaryStage.show();
+		GameManager.getInstance().setPlayer1(new UIPlayer(CellState.X));
+		GameManager.getInstance().setPlayer2(new RandomAIPlayer(CellState.O));
+		GameManager.getInstance().start();
 	}
 }
