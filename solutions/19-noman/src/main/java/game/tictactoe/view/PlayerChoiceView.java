d09dc734357dd3b1d9ca27d417405991fbb7113a
Noman5237 <anonyman637@gmail.com>
2021-07-24 12:48:46 +0600

    #4 #5 Added player choice view

---
+++ solutions/19-noman/src/main/java/game/tictactoe/view/PlayerChoiceView.java
@@ -0,0 +1,46 @@
+package game.tictactoe.view;
+
+import game.tictactoe.controller.player.Player;
+import game.tictactoe.manager.GameManager;
+import game.tictactoe.model.CellState;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.HBox;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class PlayerChoiceView extends HBox {
+	
+	private final ComboBox<Class<? extends Player>> player1Choices;
+	private final ComboBox<Class<? extends Player>> player2Choices;
+	
+	@SafeVarargs
+	public PlayerChoiceView(Class<? extends Player>... playerTypes) {
+		this.player1Choices = new ComboBox<>();
+		this.player2Choices = new ComboBox<>();
+		
+		this.player1Choices.getItems().addAll(playerTypes);
+		this.player2Choices.getItems().addAll(playerTypes);
+		
+		this.getChildren().addAll(player1Choices, player2Choices);
+		
+		this.player1Choices.setOnAction(event -> {
+			Class<? extends Player> playerClass = player1Choices.getSelectionModel().getSelectedItem();
+			GameManager.getInstance().setPlayer1(createPlayerInstance(playerClass, CellState.X));
+		});
+		
+		this.player2Choices.setOnAction(event -> {
+			Class<? extends Player> playerClass = player2Choices.getSelectionModel().getSelectedItem();
+			GameManager.getInstance().setPlayer2(createPlayerInstance(playerClass, CellState.O));
+		});
+	}
+	
+	private Player createPlayerInstance(Class<? extends Player> playerClass, CellState cellState) {
+		try {
+			return playerClass.getConstructor(CellState.class).newInstance(cellState);
+		} catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException ignored) {
+		}
+		
+		throw new RuntimeException("cannot update to the selected player type");
+	}
+	
+}
