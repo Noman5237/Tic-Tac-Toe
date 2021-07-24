66f6af7d9d8967d20c5e8e36e3c670288a53d696
Noman5237 <anonyman637@gmail.com>
2021-07-24 12:47:21 +0600

    #1 Added CellState equality for varargs

--- solutions/19-noman/src/main/java/game/tictactoe/model/CellState.java
+++ solutions/19-noman/src/main/java/game/tictactoe/model/CellState.java
@@ -3,5 +3,13 @@ package game.tictactoe.model;
 public enum CellState {
 	EMPTY,
 	X,
-	O
+	O;
+	
+	public boolean equals(CellState... cellStates) {
+		for (CellState cellState : cellStates) {
+			if (cellState != this) return false;
+		}
+		
+		return true;
+	}
 }
