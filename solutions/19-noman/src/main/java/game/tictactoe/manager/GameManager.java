8c9d74f452b8ae88d83aa375cf4071756a146c12
Noman5237 <anonyman637@gmail.com>
2021-07-24 12:51:44 +0600

    #4 #5 #6 Integrated turn based move in game Game now supports two different types of players

--- solutions/19-noman/src/main/java/game/tictactoe/manager/GameManager.java
+++ solutions/19-noman/src/main/java/game/tictactoe/manager/GameManager.java
@@ -2,6 +2,7 @@ package game.tictactoe.manager;
 
 import game.tictactoe.controller.player.Player;
 import game.tictactoe.model.Board;
+import game.tictactoe.model.CellState;
 import game.tictactoe.model.Move;
 
 public class GameManager {
@@ -15,6 +16,7 @@ public class GameManager {
 	private int player2Score;
 	private Player currentPlayer;
 	private Player lastRoundWinnerPlayer;
+	private CellState cellState;
 	
 	private GameManager() {
 		player1Score = 0;
@@ -29,9 +31,103 @@ public class GameManager {
 	}
 	
 	public void start() {
+		this.reset();
+	}
+	
+	private void restart() {
 		this.board = new Board();
-		this.currentPlayer = this.player1;
-		this.currentPlayer.promptForNextMove(this.getCurrentBoard());
+		this.currentPlayer = this.lastRoundWinnerPlayer;
+		updateBoardViewNPromptCurrentPlayerForNextMoveIfRoundRunning();
+	}
+	
+	private void reset() {
+		this.lastRoundWinnerPlayer = player1;
+		this.player1Score = 0;
+		this.player2Score = 0;
+		restart();
+	}
+	
+	private void updateBoardViewNPromptCurrentPlayerForNextMoveIfRoundRunning() {
+		Board currentBoard = this.getCurrentBoard();
+		UIManager.getInstance().getBoardView().draw(currentBoard);
+		if (this.getRoundStatus() == RoundState.RUNNING) {
+			this.currentPlayer.promptForNextMove(currentBoard);
+		} else {
+			System.out.println(this.getRoundStatus());
+		}
+	}
+	
+	private RoundState getRoundStatus() {
+		// vertical
+		for (int x = 0; x < Board.SIZE; x++) {
+			cellState = this.board.getCellState(x, 0);
+			if (cellState.equals(this.board.getCellState(x, 1), this.board.getCellState(x, 2))) {
+				if (cellState != CellState.EMPTY) {
+					if (cellState == CellState.X) {
+						return RoundState.X_WINS;
+					} else {
+						return RoundState.O_WINS;
+					}
+				}
+			}
+		}
+		
+		// horizontal
+		for (int y = 0; y < Board.SIZE; y++) {
+			cellState = this.board.getCellState(0, y);
+			if (cellState.equals(this.board.getCellState(1, y), this.board.getCellState(2, y))) {
+				if (cellState != CellState.EMPTY) {
+					if (cellState == CellState.X) {
+						return RoundState.X_WINS;
+					} else {
+						return RoundState.O_WINS;
+					}
+				}
+			}
+		}
+		
+		// diagonal
+		cellState = this.board.getCellState(0, 0);
+		if (cellState.equals(this.board.getCellState(1, 1), this.board.getCellState(2, 2))) {
+			if (cellState != CellState.EMPTY) {
+				if (cellState == CellState.X) {
+					return RoundState.X_WINS;
+				} else {
+					return RoundState.O_WINS;
+				}
+			}
+		}
+		
+		cellState = this.board.getCellState(2, 0);
+		if (cellState.equals(this.board.getCellState(1, 1), this.board.getCellState(0, 2))) {
+			if (cellState != CellState.EMPTY) {
+				if (cellState == CellState.X) {
+					return RoundState.X_WINS;
+				} else {
+					return RoundState.O_WINS;
+				}
+			}
+		}
+		
+		int emptyCellCount = countEmptyCells(this.board);
+		if (emptyCellCount > 0) {
+			return RoundState.RUNNING;
+		}
+		
+		return RoundState.DRAW;
+	}
+	
+	private int countEmptyCells(Board board) {
+		int count = 0;
+		for (int y = 0; y < Board.SIZE; y++) {
+			for (int x = 0; x < Board.SIZE; x++) {
+				if (board.getCellState(x, y) == CellState.EMPTY) {
+					++count;
+				}
+			}
+		}
+		
+		return count;
 	}
 	
 	public Board getCurrentBoard() {
@@ -39,23 +135,26 @@ public class GameManager {
 	}
 	
 	public void setNextMove(Move move) {
-		this.board.setCellState(move.getX(), move.getY(), move.getCellState());
-		if (this.currentPlayer == this.player1) {
-			this.currentPlayer = this.player2;
-		} else {
-			this.currentPlayer = this.player1;
+		if (this.getRoundStatus() == RoundState.RUNNING) {
+			this.board.setCellState(move.getX(), move.getY(), move.getCellState());
+			if (this.currentPlayer == this.player1) {
+				this.currentPlayer = this.player2;
+			} else {
+				this.currentPlayer = this.player1;
+			}
+			
+			updateBoardViewNPromptCurrentPlayerForNextMoveIfRoundRunning();
 		}
-		Board currentBoard = this.getCurrentBoard();
-		UIManager.getInstance().getBoardView().draw(currentBoard);
-		this.currentPlayer.promptForNextMove(currentBoard);
 	}
 	
 	public void setPlayer1(Player player1) {
 		this.player1 = player1;
+		this.reset();
 	}
 	
 	public void setPlayer2(Player player2) {
 		this.player2 = player2;
+		this.reset();
 	}
 	
 }
---
+++ solutions/19-noman/src/main/java/game/tictactoe/manager/RoundState.java
@@ -0,0 +1,8 @@
+package game.tictactoe.manager;
+
+public enum RoundState {
+	X_WINS,
+	O_WINS,
+	DRAW,
+	RUNNING,
+}
