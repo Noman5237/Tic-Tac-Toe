09b375cffcf463bc9e583c8a4cb1a32a2d3e1781
Noman5237 <anonyman637@gmail.com>
2021-08-19 10:54:32 +0600

    #4 #8 Activity classes for theme changer

---
+++ solutions/19-noman/src/main/java/game/tictactoe/activities/optionsactivity/Graphics.java
@@ -0,0 +1,33 @@
+package game.tictactoe.activities.optionsactivity;
+
+import game.tictactoe.managers.ApplicationManager;
+import game.tictactoe.models.Theme;
+import game.tictactoe.views.optionsview.GraphicsView;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Graphics {
+	
+	private final GraphicsView graphicsView;
+	
+	public Graphics(GraphicsView graphicsView) {
+		this.graphicsView = graphicsView;
+		this.setupThemeOptions();
+		this.setupActions();
+	}
+	
+	private void setupThemeOptions() {
+		Arrays.asList(Objects.requireNonNull(Paths.get(ApplicationManager.getInstance().getPreferencePath(), "Themes").toFile().list()))
+				.forEach(themeName -> graphicsView.getThemes().getItems().add(themeName));
+	}
+	
+	private void setupActions() {
+		this.graphicsView.getThemes().setOnAction(event -> {
+			String selectedTheme = this.graphicsView.getThemes().getSelectionModel().getSelectedItem();
+			ApplicationManager.getInstance().setTheme(Theme.loadTheme(selectedTheme));
+		});
+	}
+	
+}
---
+++ solutions/19-noman/src/main/java/game/tictactoe/activities/optionsactivity/OptionsActivity.java
@@ -0,0 +1,25 @@
+package game.tictactoe.activities.optionsactivity;
+
+import game.tictactoe.activities.Activity;
+import game.tictactoe.views.ResizableView;
+import game.tictactoe.views.optionsview.OptionsView;
+
+public class OptionsActivity implements Activity {
+	
+	private final OptionsView optionsView;
+	
+	public OptionsActivity() {
+		this.optionsView = new OptionsView();
+		new Graphics(optionsView.getGraphicsView());
+	}
+	
+	@Override
+	public ResizableView getView() {
+		return optionsView;
+	}
+	
+	@Override
+	public void onDestroy() {
+	
+	}
+}
